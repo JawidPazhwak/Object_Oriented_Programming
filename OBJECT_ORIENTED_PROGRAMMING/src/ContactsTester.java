a9239f8719f72231ad92c7b23cda898327e923e5
Jawid <jawid5839@gmail.com>
2025-02-28 21:45:31 +0600

    Update ContactsTester.java
    Added some features.

--- OBJECT_ORIENTED_PROGRAMMING/src/ContactsTester.java
+++ OBJECT_ORIENTED_PROGRAMMING/src/ContactsTester.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
 public class ContactsTester {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Contacts obj = new Contacts();
         obj.add("Bob", 1);
         obj.add("Alex", 2);
@@ -9,7 +7,7 @@ public class ContactsTester {
         obj.add("y", 4);
         obj.add("z", 5);
 
-        obj.check("Bob");
-        obj.display();
+        System.out.println(obj.check("Bob"));
+        System.out.println(obj.display());
     }
 }
