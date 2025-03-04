ef001c8a1a680d5d40c49fc45023ebd1c706c77c
Jawid <jawid5839@gmail.com>
2025-03-04 20:40:51 +0600

    Create AddTester.java
    Simple addition program in using OOP concept.➕⭕⭕🅿

---
+++ OBJECT_ORIENTED_PROGRAMMING/src/AddTester.java
@@ -0,0 +1,7 @@
+public class AddTester {
+    public static void main(String [] args){
+        Add obj1 = new Add(1, 2);
+        Add obj2 = new Add(1.2, 2.3);
+        Add obj3 = new Add("Hello", "World");
+    }
+}
