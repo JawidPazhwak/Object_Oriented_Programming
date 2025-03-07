a5b33d74e7a7e7fef8349304a9b87670245e65b9
Jawid <jawid5839@gmail.com>
2025-03-07 21:18:55 +0600

    Create Rectangle.java
    This program can calculate the the rectangle's area.

---
+++ OBJECT_ORIENTED_PROGRAMMING/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    public int length = 1;
+    public int width = 1;
+
+    public Rectangle(int length){
+        this.length = length;
+    }
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+}
